96a237d9a06cda0e60202ce26a3a4cbe33720afd
Marcello Santoro <marcelloaltermundi@gmail.com>
2022-09-21 10:04:25 -0300

    adiciona requisito 10

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
-import Missions from './components/Mission';
+import Missions from './components/Missions';
 import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
